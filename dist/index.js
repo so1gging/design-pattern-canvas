16ecadc0b376020d1c10ec5d327afaee65ff7eaa
김솔지 <so1gging@gmail.com>
2025-06-28 10:48:24 +0900

    Singleton

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
-"use strict";
-const str = "hello";
+//  import하면 Singleton
+import g1 from "./grimpan.js";
+import g2 from "./grimpan.js";
+console.log(g1.getInstance() === g2.getInstance());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-const str: string = "hello";
+//  import하면 Singleton
+import g1 from "./grimpan.js";
+import g2 from "./grimpan.js";
+
+console.log(g1.getInstance() === g2.getInstance());
