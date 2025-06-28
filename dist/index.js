732520a2754b1ee62878dc937060fd9ccd6fc533
김솔지 <so1gging@gmail.com>
2025-06-28 12:35:42 +0900

    simple factory pattern

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,21 @@
-//  import하면 Singleton
-import g1 from "./grimpan.js";
-import g2 from "./grimpan.js";
-console.log(g1.getInstance() === g2.getInstance());
+import ChromeGrimpan from "./ChromeGrimpan.js";
+import IEGrimpan from "./IEGrimpan.js";
+function grimpanFactory(type) {
+    if (type === "ie") {
+        return IEGrimpan.getInstance().initialize();
+    }
+    else if (type === "chorme") {
+        return ChromeGrimpan.getInstance().initialize();
+    }
+    else if (type === "chorme") {
+        return SatariGrimpan.getInstance().initialize();
+    }
+    else {
+        throw new Error("일치하는 type이 없습니다.");
+    }
+}
+function main() {
+    grimpanFactory("ie");
+    grimpanFactory("chrome");
+    grimpanFactory("safari");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+import ChromeGrimpan from "./ChromeGrimpan.js";
+import IEGrimpan from "./IEGrimpan.js";
 //  import하면 Singleton
-import g1 from "./grimpan.js";
-import g2 from "./grimpan.js";
+import g1 from "./ChromeGrimpan.js";
+import g2 from "./ChromeGrimpan.js";
 
-console.log(g1.getInstance() === g2.getInstance());
+function grimpanFactory(type: string) {
+  if (type === "ie") {
+    return IEGrimpan.getInstance().initialize();
+  } else if (type === "chorme") {
+    return ChromeGrimpan.getInstance().initialize();
+  } else if (type === "chorme") {
+    return SatariGrimpan.getInstance().initialize();
+  } else {
+    throw new Error("일치하는 type이 없습니다.");
+  }
+}
+
+function main() {
+  grimpanFactory("ie");
+  grimpanFactory("chrome");
+  grimpanFactory("safari");
+}
