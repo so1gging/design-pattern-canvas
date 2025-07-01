f3f1771182bae2b7cae5e363ad53e96c5008ef6d
김솔지 <so1gging@gmail.com>
2025-07-01 19:27:31 +0900

    tsc javascript

--- dist/AbstractGrimpan.js
+++ dist/AbstractGrimpan.js
@@ -1,5 +1,4 @@
 export default class Grimpan {
-    // 재정의 가능
     constructor(canvas) {
         if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
             throw new Error("canvas 엘리먼트를 입력하세요");
